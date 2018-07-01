4235a0b83a228d1c5223b45abee03ebe8b10e210
李铭昕 <715557344@qq.com>
2018-07-01 17:08:31 +0800

    增加测试单元测试

--- test/Cases/ExampleTest.php
+++ test/Cases/ExampleTest.php
@@ -1,1 +1,13 @@
 <?php
+
+namespace Swoft\Test\Cases;
+
+use Swoft\Test\AbstractTestCase;
+
+class ExampleTest extends AbstractTestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
