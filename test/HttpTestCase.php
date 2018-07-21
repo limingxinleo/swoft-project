52b8b28d70fcfcfcc0ff8d839655937d8a68595e
李铭昕 <715557344@qq.com>
2018-07-21 10:55:52 +0800

    增加用于Http接口测试的单测基类

---
+++ test/HttpTestCase.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of Swoft.
+ *
+ * @link     https://swoft.org
+ * @document https://doc.swoft.org
+ * @contact  group@swoft.org
+ * @license  https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ */
+
+namespace Swoft\Test;
+
+use PHPUnit\Framework\TestCase;
+use Swoft\HttpClient\Client;
+
+/**
+ * Class GuzzleTestCase
+ *
+ * @package Swoft\Test\Cases
+ */
+class HttpTestCase extends TestCase
+{
+    protected $client;
+
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $port = env('HTTP_PORT', 8080);
+        $this->client = new Client([
+            'base_uri' => sprintf('http://127.0.0.1:%s', $port),
+            'adapter' => 'curl',
+        ]);
+    }
+}
