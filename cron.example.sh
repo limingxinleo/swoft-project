c284b6479fc52eefe207ce781571d57febf5c306
李铭昕 <715557344@qq.com>
2018-12-14 10:40:01 +0800

    Update cron.example.sh

--- cron.example.sh
+++ cron.example.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 basepath=$(cd `dirname $0`; pwd)
+docker pull registry.cn-shanghai.aliyuncs.com/limingxinleo/swoft-project:latest
 docker run --rm -i -v $basepath/.env:/opt/www/.env \
 --entrypoint php registry.cn-shanghai.aliyuncs.com/limingxinleo/swoft-project:latest \
 /opt/www/bin/swoft app:init
